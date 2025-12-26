0d7e6e60cf9ba83a29d5135ca632d9d53b56188e
Manohar <lovelysaimanohar143@gmail.com>
2025-12-26 08:07:27 +0530

    Update main.py

--- Turtle Capstone/main.py
+++ Turtle Capstone/main.py
@@ -4,11 +4,43 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
+
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player=Player()
+manager=CarManager()
+manager.hideturtle()
+score=Scoreboard()
+
+screen.onkey(player.up, "Up")
+
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    manager.create_car()
+    manager.move_car()
+    
+    #Detect collision with car
+    for i in manager.all_cars:
+        if player.distance(i) < 20 :
+            
+            game_is_on =False
+   
+    #Detect turtle crossed the path
+    if player.is_at_finish_line():
+        player.go_to_start()
+        manager.increase_speed()
+        score.level_up()
+
+
+
+
+screen.exitonclick()
