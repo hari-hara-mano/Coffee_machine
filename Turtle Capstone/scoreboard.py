7dd797aed5340e9ca00b40e950b5c88ab00516ad
Manohar <lovelysaimanohar143@gmail.com>
2025-12-26 08:07:36 +0530

    Update scoreboard.py

--- Turtle Capstone/scoreboard.py
+++ Turtle Capstone/scoreboard.py
@@ -1,5 +1,25 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+FONT = ("Arial", 18, "bold")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level= 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-260,260)
+        self.rite()
+
+    def level_up(self):
+        self.clear()
+        self.level += 1
+        self.rite()
+
+    def rite(self):
+        self.write(f"Level:{self.level}", align="left", font=FONT)
+
+    def g_over(self):
+        self.home()
+        self.write(f" GAME OVER ", align="center", font=FONT)
