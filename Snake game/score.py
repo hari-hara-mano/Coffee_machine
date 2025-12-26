778752ec55c038e3e29980a6303de36c6df3a840
Manohar <lovelysaimanohar143@gmail.com>
2025-12-26 08:06:55 +0530

    Update score.py

--- Snake game/score.py
+++ Snake game/score.py
@@ -10,28 +10,32 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        with open("data.txt") as data:
+            self.high_score=int(data.read())
         self.penup()
         self.color("white")
         self.goto(0, 260) 
-        self.shape(None)  
+        self.shape(None)    
         self.hideturtle()    
         self.update()
         
 
     def update(self):
+        self.clear()
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align=ALIGNMENT, font= FONT)
 
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font= FONT)
+    def g_reset(self):
+        if self.high_score <  self.score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
 
-    def game_over(self):
+        self.score=0
+        self.update()
 
-        self.color("yellow")
-        self.goto(0,0)
-        self.write(f" Game Over ", align=ALIGNMENT, font= FONT)
-        screen.exitonclick()
 
     def increase(self):
         self.score+=1
-        self.clear()
         self.update()
 
 
